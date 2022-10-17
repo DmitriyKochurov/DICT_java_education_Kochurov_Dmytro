ddb7e5388e0703556dc9cb297866568043759578
DmitriyKochurov <dimon7932rtek@gmail.com>
2022-10-17 10:09:18 +0300

    ChatBot 2-nd Stage

--- src/ChatBot/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot/ChatBot.java
@@ -1,8 +1,16 @@
 package ChatBot.ChatBot;
+import java.util.Scanner;
 
 public class ChatBot {
     public static void main(String[] args) {
+
+        Scanner input= new Scanner(System.in);
+
         System.out.println("Hello, my name is HIMARS.");
         System.out.println("I was created in 2022 year.");
+        System.out.print("Please remind me your name: ");
+        String x = input.next();
+        input.close();
+        System.out.println("What a great name you have," + x);
     }
 }
