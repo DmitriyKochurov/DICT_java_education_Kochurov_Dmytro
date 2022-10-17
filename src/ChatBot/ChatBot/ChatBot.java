be51f2f30a81e13d73112271bf904c9bde914e4f
DmitriyKochurov <dimon7932rtek@gmail.com>
2022-10-17 10:47:51 +0300

    ChatBot 3-rd Stage

--- src/ChatBot/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot/ChatBot.java
@@ -10,7 +10,13 @@ public class ChatBot {
         System.out.println("I was created in 2022 year.");
         System.out.print("Please remind me your name: ");
         String x = input.next();
-        input.close();
         System.out.println("What a great name you have," + x);
+        System.out.println("Let me guess your age.");
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7");
+        int r3 = input.nextInt();
+        int r5 = input.nextInt();
+        int r7 = input.nextInt();
+        int age = (r3 * 70 + r5 * 21 + r7 * 15) % 105;
+        System.out.println("Your age is " + age + " that`s a good time to start programming!");
     }
 }
