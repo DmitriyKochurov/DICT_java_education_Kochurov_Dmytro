429a2508f5c2299bfe574c3720245ee91767e2a3
DmitriyKochurov <dimon7932rtek@gmail.com>
2022-10-17 09:50:14 +0300

    ChatBot 1-st Stage

---
+++ src/ChatBot/ChatBot/ChatBot.java
@@ -0,0 +1,8 @@
+package ChatBot.ChatBot;
+
+public class ChatBot {
+    public static void main(String[] args) {
+        System.out.println("Hello, my name is HIMARS.");
+        System.out.println("I was created in 2022 year.");
+    }
+}
